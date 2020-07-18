c01e01126cacb8d5beb0745f28bff99c30c7238e
degenerate069 <65444142+degenerate069@users.noreply.github.com>
2020-07-18 09:21:34 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
+
+  function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+    
+}
